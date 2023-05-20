38ad47b60f2e47a8fd44bbdfa4ee9ad792b3ee9f
Davit <davitkhachikyan93@gmail.com>
2023-05-20 21:35:56 +0400

    added homework2

---
+++ app.js
@@ -0,0 +1,18 @@
+import {faker} from '@faker-js/faker';
+import  lodash from 'lodash';
+
+let users = [];
+
+for(let i = 0; i <=10; i++) {
+    users.push({
+        'name': faker.person.firstName(),
+        'phone': faker.phone.number('###-##-##-##'),
+        'email': faker.internet.email()
+    }) ;
+}
+
+console.log(users);
+
+let sortedUsers = lodash.sortBy(users, 'name');
+
+console.log(sortedUsers);
